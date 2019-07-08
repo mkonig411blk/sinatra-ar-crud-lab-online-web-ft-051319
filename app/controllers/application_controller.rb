a1d2163f31863505be335a190a6979e43c1ff836
Margot Konig <margot.konig@blackrock.com>
2019-07-08 19:57:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,49 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  post '/articles' do 
+    @article = Article.create(params)
+    id = @article.id
+    redirect "/articles/#{id}"
+  end 
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params["id"])
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params["id"])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do 
+    id = params["id"]
+    new_params = {}
+    old_article = Article.find(id)
+    new_params[:title] = params["title"]
+    new_params[:content] = params["content"]
+    old_article.update(new_params)
+    # @article = Article.find(params[:id])
+    # @article.update(params[:edit])
+    # @article.save
+    redirect "/articles/#{id}"
+  end 
+  
+  delete '/articles/:id/delete' do
+    @article = Article.find(params["id"])
+    @article.destroy
+    erb :delete
+  end
 end
+
